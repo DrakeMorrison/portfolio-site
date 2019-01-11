2d309dfea2e7091b6713abad0bf9079d25477f6c
Drake Morrison <drakermorrison@gmail.com>
2019-01-11 12:20:28 -0600

    About Me CSS partially figured out

---
+++ src/App/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {Route, BrowserRouter, Switch}  from 'react-router-dom';
+
+import './App.css';
+
+import AboutMe from '../components/AboutMe/AboutMe';
+import Projects from '../components/Projects/Projects';
+import Contact from '../components/Contact/Contact';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='cover-container'>
+          <BrowserRouter>
+            <Switch>
+              <Route
+                path='/'
+                exact
+                component={AboutMe}
+              />
+              <Route
+                path='/projects'
+                component={Projects}
+              />
+              <Route
+                path='/contact'
+                component={Contact}
+              />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
