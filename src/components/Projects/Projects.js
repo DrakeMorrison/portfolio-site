9b3a9297ae42ef96c966f0cbd597c15e8921bd8b
Drake Morrison <drakermorrison@gmail.com>
2019-02-22 10:47:10 -0600

    slider working

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import bgHelper from '../../bgHelper';
 import projectRequests from '../../firebaseRequests/projectRequests';
+import Slider from 'react-slick';
 
 class Projects extends React.Component {
   state = {
@@ -22,6 +23,33 @@ class Projects extends React.Component {
   }
 
   render () {
+
+    const projectCards = this.state.projects.map(project => {
+      return (
+        <div class="media">
+          <div class="media-left">
+            <img class="media-object img-responsive img-rounded" src={project.thumbnail} alt="..." />
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">{project.title}</h4>
+            {project.description}
+          </div>
+        </div>
+      );
+    });
+
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      pauseOnHover: true,
+      autoPlaySpeed: 3000
+    };
+
     return (
       <div className='AboutMe'>
 
@@ -45,7 +73,9 @@ class Projects extends React.Component {
 
         <div className='inner cover'>
 
-          <p>Projects Here: </p>
+          <Slider {...settings}>
+            {projectCards}
+          </Slider>
 
         </div>
 
